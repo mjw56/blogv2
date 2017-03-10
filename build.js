67130180772bb9928f2057c154d35251e8d620d4
Mike Wilcox <mwilcox56@gmail.com>
2017-03-10 17:47:47 -0500

    read handlebars template, write files.

--- build.js
+++ build.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
+const handlebars = require('handlebars');
 const showdown = require('showdown');
 const converter = new showdown.Converter();
+
+function mkdirSync(path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
+}
+
+function clearDirSync(dirPath) {
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+
+  fs.rmdirSync(dirPath);
+};
 
 function readFiles(dirname, onFileContent, onError) {
   fs.readdir(dirname, function(err, filenames) {
@@ -8,14 +32,37 @@ function readFiles(dirname, onFileContent, onError) {
       onError(err);
       return;
     }
+
+    // delete all the old files in public
+    clearDirSync('public');
+    mkdirSync('public');
+
+    var base = fs.readFileSync('_base.html', 'utf8');
+
     filenames.forEach(function(filename) {
       fs.readFile(`${dirname}/${filename}`, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
           return;
         }
-        // onFileContent(filename, content);
-        console.log(converter.makeHtml(content));
+
+        const context = {
+          body: converter.makeHtml(content)
+        };
+
+        const template = handlebars.compile(base);
+        const html = template(context);
+
+        const name = filename.replace('.md', '');
+        // we have created the new html file, write it to the
+        // public directory with a time stamp on the file
+        fs.writeFile(`public/${name}.html`, html, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+
+            console.log(`${name}.html was saved!`);
+        });
       });
     });
   });
