4c3ee1fa02cfca33dbb6b0776a48b2943007acee
Mike Wilcox <mwilcox56@gmail.com>
2017-03-13 22:10:40 -0400

    add open browser and watch support.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const watch = require('node-watch');
+const openBrowser = require('./src/build/openBrowser.js');
+var exec = require('child_process').exec;
 
 const app = express();
 
@@ -11,4 +14,12 @@ const server = http.createServer(app);
 server.listen(3000, () => {
     console.log(`==> Server is listening on port 3000`);
     console.log(`==> 🌎  Go to localhost:3000`);
+
+    openBrowser('http://localhost:3000');
+});
+
+watch('./src', { recursive: true }, function(evt, name) {
+  exec('yarn run build', function callback(error, stdout, stderr){
+    openBrowser('http://localhost:3000');
+  });
 });
